d60b553809a16ac4d0df875cfcf71efacf1ab1c0
Max Mitschke <max@movact.de>
2020-12-22 16:46:33 +0100

    update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry, Platform } from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(Platform.OS === 'ios' ? 'voteswiper' : appName, () => App);
+AppRegistry.registerComponent(
+  Platform.OS === 'ios' ? 'voteswiper' : appName,
+  () => App,
+);
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,13 +1,8 @@
 // import NavigationStore from './NavigationStore';
-import { AsyncStorage } from 'react-native';
-import { create } from 'mobx-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {create} from 'mobx-persist';
 import AppStore from './AppStore';
 import SwiperStore from './SwiperStore';
-
-const hydrate = create({
-  storage: AsyncStorage,
-  jsonify: true,
-});
 
 const hydrateApp = create({
   storage: AsyncStorage,
@@ -19,13 +14,11 @@ const app = new AppStore();
 
 export default {
   app,
-  swiper
+  swiper,
 };
 
-hydrateApp('app', app)
-  .then(() => {
-
-    setTimeout(() => {
-      app.setHydrated();
-    }, 500);
-  });
+hydrateApp('app', app).then(() => {
+  setTimeout(() => {
+    app.setHydrated();
+  }, 500);
+});
