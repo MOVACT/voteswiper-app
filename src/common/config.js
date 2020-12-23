bbfc08c3838999d9d8bab7ef720fc0c748044b32
Max Mitschke <max@movact.de>
2020-12-23 13:26:15 +0100

    enable hermes

--- src/common/config.js
+++ src/common/config.js
@@ -1,7 +1,24 @@
 const config = {
   apiUrl: 'https://www.voteswiper.org/!',
   fallbackLocale: 'en',
-  locales: ['en', 'de', 'cs', 'da', 'et', 'el', 'es', 'fi', 'fr', 'hu', 'it', 'lt', 'pl', 'ro', 'sl', 'sv']
-}
+  locales: [
+    'en',
+    'de',
+    'cs',
+    'da',
+    'et',
+    'el',
+    'es',
+    'fi',
+    'fr',
+    'hu',
+    'it',
+    'lt',
+    'pl',
+    'ro',
+    'sl',
+    'sv',
+  ],
+};
 
 export default config;
