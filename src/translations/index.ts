ea338b8c35b40566ee42043d76e768116649b7da
Max Mitschke <max@movact.de>
2021-06-14 20:03:58 +0200

    fix translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,37 +1,15 @@
-import en from './en';
 import de from './de';
-import cs from './cs';
-import sv from './sv';
+import en from './en';
 import fi from './fi';
-import hu from './hu';
 import fr from './fr';
-import pl from './pl';
-import el from './el';
-import da from './da';
-import it from './it';
-import ro from './ro';
-import et from './et';
-import lt from './lt';
-import es from './es';
-import sl from './sl';
+import sv from './sv';
 
 const lang = {
   en: en,
   de: de,
-  cs: cs,
-  da: da,
   fi: fi,
   fr: fr,
-  pl: pl,
   sv: sv,
-  it: it,
-  hu: hu,
-  ro: ro,
-  et: et,
-  lt: lt,
-  el: el,
-  es: es,
-  sl: sl,
 };
 
 export default lang;
